98994b39ef931d43ce0166088e7487b7a29ab376
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-07-30 14:21:33 +0300

    conflict fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,8 @@ import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 
+const todoItem = ['Create new app', 'Get new job', 'Find new friends', 'Learn new language'];
+
 const App = () => ( <div>
   <h1>TO DO LIST</h1>
   <InputItem />

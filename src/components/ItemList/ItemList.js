cb1753310c6a158a53ce0092f2ec8b811001ecc3
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-07-31 14:13:49 +0300

    parentheses added

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from '../Item/Item';
 
 const ItemList = ({ items }) => (<ul>
-    {items.map(item => <li key={item.value}>
+    {items.map((item) => <li key={item.value}>
         <Item value={item.value} />
     </li>)}
 </ul>);

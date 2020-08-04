8d09ae6aa9f4d1e8cdd8c2c8f7e12c67c12298e7
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-08-04 13:22:58 +0300

    state added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,34 +5,36 @@ import ItemList from '../ItemList/ItemList';
 import styles from "./App.module.css";
 
 class App extends React.Component {
-  render() {
-    const items = [
-    {
-      value: "Create new app",
-      isDone: true
-    },
-    {
-      value: "Get new job",
-      isDone: false
-    },
-    {
-      value: "Find new friends",
-      isDone: true
-    },
-    {
-      value: "Learn new language",
-      isDone: false
-    }
-  ];
+  state = {
+    items: [
+      {
+        value: "Create new app",
+        isDone: true
+      },
+      {
+        value: "Get new job",
+        isDone: false
+      },
+      {
+        value: "Find new friends",
+        isDone: true
+      },
+      {
+        value: "Learn new language",
+        isDone: false
+      }
+    ]
+  };
 
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>TO DO LIST</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count={4} />
-    </div>);
-  }
+  render() {
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>TO DO LIST</h1>
+        <InputItem />
+        <ItemList items={this.state.items} />
+        <Footer count={4} />
+      </div>);
+    }
 };
 
 export default App;

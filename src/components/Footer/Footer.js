c2c79cd0d68b5439abe1da3b8dc6c5d2c85126c7
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-08-24 23:17:32 +0200

    props added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,11 @@ import styles from "./Footer.module.css";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
-const Footer = ({ count }) => (
+const Footer = ({ count, doneCount }) => (
 <div>
     <div className={styles.text}>
-        We have { count } more tasks to do. Start working!
+        <p>We have { count } more tasks to do. Start working!</p>
+        <p>But you have already done { doneCount } tasks! Great job!</p>
     </div>
     <div className={styles.tabs}>
         <Tabs
@@ -22,5 +23,10 @@ const Footer = ({ count }) => (
     </div>
 </div>
 );
+
+Footer.defaultProps = {
+    count: 0,
+    doneCount: 666
+}
 
 export default Footer;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from "./ItemList.module.css";
 
-const ItemList = ({ items, onClickDone, onClickDelete }) => (<ul>
+const ItemList = ({items, onClickDone, onClickDelete }) => (<ul>
     {items.map((item) => <li key={item.value} className={styles.list}>
         <Item 
             value={item.value} 

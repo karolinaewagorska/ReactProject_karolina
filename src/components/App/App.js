1f0bc08d91fab7a04f750edadb0dd31dcacc91ce
KarolinaEwa <gorska.karoliewa@gmail.com>
2020-09-01 13:04:58 +0200

    react router added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,107 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Footer from '../Footer/Footer';
-import InputItem from '../InputItem/InputItem';
-import ItemList from '../ItemList/ItemList';
-import styles from "./App.module.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const App = () => {
-  const initialState = {
-    items: [
-      {
-        value: "Create new app",
-        isDone: true,
-        id: 1
-      },
-      {
-        value: "Get new job",
-        isDone: false,
-        id: 2
-      },
-      {
-        value: "Find new friends",
-        isDone: true,
-        id: 3
-      },
-      {
-        value: "Learn new language",
-        isDone: false,
-        id: 4
-      }
-    ],
-    count: 4,
-    error: false
-  };
+import Todo from "../Todo/Todo";
+import About from "../About/About";
+import Contacts from "../Contacts/Contacts";
+import styles from "./App.module.css";
 
-  const [items, setItems] = useState(initialState.items);
-  const [count, setCount] = useState(initialState.count);
-  const [error, setError] = useState(initialState.error);
+const App = () => (
+  <Router>
+    <div className={styles.wrap}>
+      <Card className={styles.sidebar}>
+        <MenuList>
+          <Link to ="/" className={styles.link}><MenuItem>About</MenuItem></Link>
+          <Link to="/todo" className={styles.link}><MenuItem>Tasks</MenuItem></Link>
+          <Link to="/contacts" className={styles.link}><MenuItem>Contacts</MenuItem></Link>
+        </MenuList>
+      </Card>
 
-  useEffect(() => {
-    console.log("componentDidUpdate"); /* eslint-disable-line no-console */
-  });
+      <Card className={styles.content}>
+        <Route path="/" exact component={About} />
+        <Route path="/todo" component={Todo} />
+        <Route path="/contacts" component={Contacts} />
+      </Card>
 
-  useEffect(() => {
-    console.log("componentDidMount"); /* eslint-disable-line no-console */
-  }, []);
-
-  const onClickDone = (id) => {
-    const newItemList = (items.map((item) => {
-      const newItem = { ...item };
-      if (item.id === id) {
-        newItem.isDone = !item.isDone;
-      }
-      return newItem;
-    })
-    );
-    setItems(newItemList);
-  };
-
-  const onClickDelete = (id) => {
-    const newItems = items.filter((item) => item.id !== id);
-    setItems(newItems);
-    setCount((count) => count - 1);
-  };
-
-
-  const onClickAdd = (value) => {
-    if (value === "") {
-      setError((error) => true);
-    } else {
-     const newItems = [
-      ...items,
-        {
-          value,
-          isDone: false,
-          id: count + 1
-        } 
-    ];
-    setItems(newItems);
-    setCount((count) => count + 1);
-    }
-  };
-  
-    return (
-      <div className={styles.wrap}>
-        <Card>
-          <CardContent>
-            <h1 className={styles.title}>TO DO LIST</h1>
-            <InputItem 
-              onClickAdd={onClickAdd} 
-              error={error}
-            />
-            <ItemList 
-              items={items} 
-              onClickDone={onClickDone} 
-              onClickDelete={onClickDelete}
-            />
-            <Footer 
-              count={count} 
-            />
-          </CardContent>
-        </Card>
-      </div>);
-};
+    </div>
+  </Router>)
 
 export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import Footer from '../Footer/Footer';
+import InputItem from '../InputItem/InputItem';
+import ItemList from '../ItemList/ItemList';
+import CardContent from "@material-ui/core/CardContent";
+import styles from "./Todo.module.css";
+
+const Todo = () => {
+  const initialState = {
+    items: [
+      {
+        value: "Create new app",
+        isDone: true,
+        id: 1
+      },
+      {
+        value: "Get new job",
+        isDone: false,
+        id: 2
+      },
+      {
+        value: "Find new friends",
+        isDone: true,
+        id: 3
+      },
+      {
+        value: "Learn new language",
+        isDone: false,
+        id: 4
+      }
+    ],
+    count: 4,
+    error: false
+  };
+
+  const [items, setItems] = useState(initialState.items);
+  const [count, setCount] = useState(initialState.count);
+  const [error, setError] = useState(initialState.error);
+
+  useEffect(() => {
+    console.log("componentDidUpdate"); /* eslint-disable-line no-console */
+  });
+
+  useEffect(() => {
+    console.log("componentDidMount"); /* eslint-disable-line no-console */
+  }, []);
+
+  const onClickDone = (id) => {
+    const newItemList = (items.map((item) => {
+      const newItem = { ...item };
+      if (item.id === id) {
+        newItem.isDone = !item.isDone;
+      }
+      return newItem;
+    })
+    );
+    setItems(newItemList);
+  };
+
+  const onClickDelete = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+    setCount((count) => count - 1);
+  };
+
+
+  const onClickAdd = (value) => {
+    if (value === "") {
+      setError((error) => true);
+    } else {
+     const newItems = [
+      ...items,
+        {
+          value,
+          isDone: false,
+          id: count + 1
+        } 
+    ];
+    setItems(newItems);
+    setCount((count) => count + 1);
+    }
+  };
+  
+    return (
+        <CardContent>
+            <h1 className={styles.title}>TO DO LIST</h1>
+            <InputItem 
+              onClickAdd={onClickAdd} 
+              error={error}
+            />
+            <ItemList 
+              items={items} 
+              onClickDone={onClickDone} 
+              onClickDelete={onClickDelete}
+            />
+            <Footer 
+              count={count} 
+            />
+        </CardContent>
+    )
+};
+
+export default Todo;
